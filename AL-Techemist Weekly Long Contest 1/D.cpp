ae23e70b3355b036e901c08ea88faaff7cc43ab6
Abdur-Rawf <rawf.ict20@gmail.com>
2024-11-23 17:08:47 +0600

    Add files via upload

---
+++ AL-Techemist Weekly Long Contest 1/D.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int t;
+   cin>>t;
+   if(t==2)
+    cout<<"NO\n";
+   else if(t%2==0)
+    cout<<"YES\n";
+   else
+    cout<<"NO\n";
+    return 0;
+}
+
+
