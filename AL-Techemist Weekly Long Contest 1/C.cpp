904e237c4f4c557126d460ca1a44e189519427b1
Abdur-Rawf <rawf.ict20@gmail.com>
2024-11-23 17:07:04 +0600

    Add files via upload

---
+++ AL-Techemist Weekly Long Contest 1/C.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+solveMeFirst(int a,int b)
+    {
+        sum=a+b;
+        return sum;
+    }
+int main()
+{
+    int a,b,sum;
+    cin>>a>>b;
+    solveMeFirst(a,b);
+    cout<<sum<<endl;
+    return 0;
+}
+
