2e225eca233977ae835e259146c68945198c526c
Abdur-Rawf <rawf.ict20@gmail.com>
2025-02-27 10:59:46 +0600

    Add files via upload

---
+++ 52 Programming Problem by Tamim Shahriar Subin/Problem 1 - Even Odd 1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n;
+    cin>>t;
+    while(t--){
+       cin>>n;
+       if(n%2==0)
+            cout<<"even\n";
+       else
+        cout<<"odd\n";
+    }
+    return 0;
+}
+
